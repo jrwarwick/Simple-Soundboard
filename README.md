1ac07a409cee8494f3b6b5b77baf299232c6c888
Justin Warwick <justin.warwick@gmail.com>
2016-03-06 00:45:38 -0800

    Generalize, add more audio formats, reset button, add background loop and button for it. Rename sb.css . Explicit license.

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # Simple Single Page Soundboard
-This code creates a Soundboard on a single page using PHP, Javascript, JQuery, HTML5 and CSS. I was motivated to create when Soundboard.com ignored my two feature requests. This version is better than Soundboard.com in at least 4 ways:
+This code creates a Soundboard on a single page using PHP, Javascript, JQuery, HTML5 and CSS. Original author was motivated to create when Soundboard.com ignored two feature requests. This version is better than Soundboard.com in at least 4 ways:
   1. Loads way faster.
   2. Tablet and Mobile friendly. 
   3. Sorts alphabetically automatically.  
-  4. You can play multiple drops simultaneously. 
+  4. You can play multiple audio samples simultaneously. 
+  5. Independent background loop.
 
 ### Getting Started
-  1. Create a new folder and place all the MP3 drops you wish to have on the soundboard.
-  2. On the top of the index.php file define that folder where is says $mp3directory. 
+  1. Create a new folder and place all the audio files you wish to have on the soundboard.
+  2. On the top of the index.php file define that folder where is says $audiodirectory. 
   3. Deploy to a web server that supports PHP (most do). 
 
 ### Some Ideas For Your Soundboard
-  1. Search for "CSS Button Generator" to create your own custom super cool looking buttons. Place that CSS into the sb.css file.  
-  2. I set the preload: none. This is because I have 200+ drops and want the initial page draw to be fast. Remove this line if you prefer the drops load on page draw and not on play. 
+  1. Search for "CSS Button Generator" to create your own custom super cool looking buttons. Place that CSS into the soundboard.css file.  
+  2. I set the preload: none. This is because I have 200+ audio files and want the initial page draw to be fast. Remove this line if you prefer the drops load on page draw and not on play. 
   
-### Demo
-  This is the [Neil Rogers Soundboard](http://neilrogers.org/soundboard/) built using this code. 
+### Credits
+Original by digitalcolony
+Modifications by JRWarwick
 
-
-  
-
+### License
+Original license assumed to be MIT, and this edition also MIT licensed. Refer to LICENSE.txt .
 
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-	$mp3 = array();
-	$mp3directory = 'neil';
+	$audio = array();
+	$audiodirectory = 'audio';
 	
-	// build an array of MP3 files 
-	$directory = new DirectoryIterator($mp3directory);
+	// build an array of audio files 
+	$directory = new DirectoryIterator($audiodirectory);
 	foreach ($directory as $fileinfo) {
 		if ($fileinfo->isFile()) {
 			$extension = $fileinfo->getExtension();
-			if($extension == "mp3"){
-				$mp3[] = $fileinfo->getFilename();
+			if($extension == "mp3"||$extension == "ogg"||$extension == "m4a"){
+				$audio[] = $fileinfo->getFilename();
 			}
 		}
 	}
@@ -16,13 +16,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+  	<title>Soundboard</title>
   	<meta charset="utf-8">
-  	<title>Neil Rogers Soundboard</title>
   	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  	<meta name="description" content="Neil Rogers Soundboard">
+  	<meta name="description" content="Soundboard">
   	<meta name="viewport" content="width=device-width">
+	<meta property="og:title" content="Soundboard" />
+	<meta property="og:image" content="/img/soundboard.jpg" />	
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>	  
-  	<link rel="stylesheet" type="text/css" href="sb.css">
+  	<link rel="stylesheet" type="text/css" href="soundboard.css">
   	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">   
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
@@ -31,52 +33,66 @@
 	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
 	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
 	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" /> 
-	<meta property="og:title" content="Soundboard for Radio's Neil Rogers" />
-	<meta property="og:image" content="/img/neil-mask-radio.jpg" />	
 </head>
 
 <body>
 <div id="soundboard">
 </div>	
   
-<script type="text/javascript">
-	// pass PHP array of MP3 files declared above to JavaScript variable
-	var ar = <?php echo json_encode($mp3) ?>;
-	var BASE_AUDIO_PATH = '' + <?php echo json_encode($mp3directory) ?> + '/';
+	<script type="text/javascript">
+		// pass PHP array of audio files declared above to JavaScript variable
+		var ar = <?php echo json_encode($audio) ?>;
+		var BASE_AUDIO_PATH = '' + <?php echo json_encode($audiodirectory) ?> + '/';
 
-	// sort files to be in alphabetical order by title
-	ar.sort(function (a, b) {
-	    return a.toLowerCase().localeCompare(b.toLowerCase());
-	});
+		// sort files to be in alphabetical order by title
+		ar.sort(function (a, b) {
+		    return a.toLowerCase().localeCompare(b.toLowerCase());
+		});
 
-	// Add <audio> files and buttons to soundboard
-	// preload set to "none" is optional
-	ar.map(function(url){
-		$('#soundboard').append(function(){
-			var thisAudio = $('<audio/>').attr({
-				src: BASE_AUDIO_PATH + url,
-				preload: "none",
-				onplay:"$(this).siblings('button').css('color', 'yellow');",
-				onended: "$(this).siblings('button').css('color', 'white');"	
-				}) 	
-			var buttonText = url.replace(/-/g, ' ').replace('.mp3', '').replace('[Q]','?');			
-			var thisButton = $('<button />').addClass('myButton').text(buttonText);			 		
-			return $('<span />').addClass('track').append(thisAudio).append(thisButton);
-		})
-	});
-	$('#soundboard').on( 'click', 'button', function() {			
-		$(this).siblings('audio').get(0).play();							
-	});				
+		// Add <audio> files and buttons to soundboard
+		// preload set to "none" is optional
+		ar.map(function(url){
+			$('#soundboard').append(function(){
+				var thisAudio = $('<audio/>').attr({
+					src: BASE_AUDIO_PATH + url,
+					preload: "none",
+					onplay:"$(this).siblings('button').css('color', 'yellow');",
+					onended: "$(this).siblings('button').css('color', 'white');"	
+					}) 	
+				var buttonText = url.replace(/[-_]/g, ' ').replace('.mp3', '').replace('.ogg','').replace('.m4a','').replace('[Q]','?');			
+				var thisButton = $('<button />').addClass('myButton').text(buttonText);			 		
+				return $('<span />').addClass('track').append(thisAudio).append(thisButton);
+			})
+		});
+		$('#soundboard').on( 'click', 'button', function() {			
+			$(this).siblings('audio').get(0).play();							
+		});				
 	</script>
 
+	<audio id="audioBackground1" autoplay loop><!-- https://developer.mozilla.org/en-US/docs/Games/Techniques/Audio_for_Web_Games -->
+		<source src="audio/background.ogg" type="audio/ogg" />
+	</audio>
 <div id="footer">
-	<p><strong><a href="/">HOME</a></strong></p>
+	<p><strong><a href="./"><button id="reset" />Reset</button></a>
+	<p><button id="backgroundToggle">Background  &Vert; / &vrtri; </button>
 </div>
         <script>
-            var _gaq = [['_setAccount', 'UA-1744925-10'], ['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
+		$("#backgroundToggle").click(function() {
+			var bga = $("#audioBackground1");
+			if (bga.prop("paused") == false) {
+				bga.trigger("pause");
+			} else {
+				bga.trigger("play");
+			}
+		});
+
+		$(document).ready(function(){
+			$("audio").each(function(idx){
+				console.log(idx+ " " + this.src);
+			});
+		});
+
+		//And finally Google analytics or other load-ups if you need them.
         </script>
 </body>
 </html>
